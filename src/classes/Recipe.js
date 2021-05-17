cdc700476a016771bec7015fea71f52294d30d1a
Kat White <kat.white.a@gmail.com>
2021-05-17 17:33:11 -0600

    Complete instantiation for Recipe test and troubleshoot testing

---
+++ src/classes/Recipe.js
@@ -0,0 +1,12 @@
+class Recipe {
+    constructor(id, imageUrl, ingredients, instructions, name, tags) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.ingredients = ingredients;
+        this.instructions =  instructions;
+        this.name = name;
+        this.tages = tags;
+    }
+}
+
+export default Recipe
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -13,32 +13,30 @@ describe('Recipe', () => {
     let recipe;
 
     beforeEach('Instantiate recipe', () => {
-        ingredient1 = {"id": 20081, "name": "wheat flour","estimatedCostInCents": 142}
-        ingredient2 = {"id": 18372,"name": "bicarbonate of soda", "estimatedCostInCents": 582}
-        ingredients = [ingredient1, ingredient2]
-        instruction1 = {"instruction": "Preheat oven to 170 – 200°F", "number": 1},
-        instruction2 = {"Mix warm water with brown rice syrup, molasses", "number": 2}
-        instructions = [instruction1, instruction2]
-        name = "Whole Grain Bread"
-        tags = ["bread"]
-        recipe = new Recipe(1234, "https://spoonacular.com/recipeImages/741603-556x370.jpeg", ingredients, instructions, name, tags)
+        ingredient1 = {"id": 20081, "name": "wheat flour","estimatedCostInCents": 142};
+        ingredient2 = {"id": 18372,"name": "bicarbonate of soda", "estimatedCostInCents": 582};
+        ingredients = [ingredient1, ingredient2];
+        instruction1 = {"instruction": "Preheat oven to 170 – 200°F", "number": 1};
+        instruction2 = {"instruction": "Mix warm water with brown rice syrup, molasses", "number": 2};
+        instructions = [instruction1, instruction2];
+        name = "Whole Grain Bread";
+        tags = ["bread"];
+        recipe = new Recipe(1234, "https://spoonacular.com/recipeImages/741603-556x370.jpeg", ingredients, instructions, name, tags);
     });
 
     it('Should be a function', () => {
-     expect(Recipe).to.be.a('function');
+        expect(Recipe).to.be.a('function');
     });
 
     it('Should be an instance of Recipe', () => {
      expect(recipe).to.be.an.instanceof(Recipe);
     });
 
-    
+    it('Should have an ID', () => {
+        expect(recipe.id).to.equal(1234)
+    });
 
-
-    
-// id: number
-// image: url
-// ingredients: array of ingredient objects
-// instructions: array of instruction objects
-// name: recipe name
-// tags: array of string tags
+    it('Should have an ID that is a number', () => {
+        expect(recipe.id).to.be.a('number')
+    });
+});
