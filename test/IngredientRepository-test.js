8dbe8454f0c8942c2e477bf3c62a061a24a22cc8
Lauren Kessell <lkessell1@gmail.com>
2021-05-18 16:31:11 -0400

    Set up Ingredient Repo files

---
+++ test/IngredientRepository-test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import IngredientRepository from '../src/classes/IngredientRepository';
+import Ingredient from '../src/classes/Ingredient';
+
+describe('Ingredient Repository', () => {
+
+  let ingredientRepo;
+  let ingredient1;
+  let ingredient2;
+
+  beforeEach('Instantiate classes', () => {
+    ingredient1 = new Ingredient(20081, 'wheat flour', 142);
+    ingredient2 = new Ingredient(18372, 'bicarbonate of soda', 582);
+    ingredientRepo = new IngredientRepository([ingredient1, ingredient2]);
+  })
+
+  it('Should be a function', () => {
+    expect(IngredientRepository).to.be.a('function');
+  });
+
+  it('Should be an instance of IngredientRepository', () => {
+    expect(ingredientRepo).to.be.an.instanceof(IngredientRepository);
+  });
+
+})
