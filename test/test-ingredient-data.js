c254e8138157d06a596e4b62b17a4c4964c40d14
Lauren Kessell <lkessell1@gmail.com>
2021-05-20 21:34:13 -0400

    Update test data files

--- test/test-ingredient-data.js
+++ test/test-ingredient-data.js
@@ -1,8 +1,13 @@
-const ingredient1 = {"id": 20081, "name": "wheat flour","estimatedCostInCents": 142};
-const ingredient2 = {"id": 18372,"name": "bicarbonate of soda", "estimatedCostInCents": 582};
-const ingredient3 = {"id": 12345,"name": "fake crab", "estimatedCostInCents": 213};
-const ingredient4 = {"id": 54321,"name": "ritz crackers", "estimatedCostInCents": 300};
+// const ingredient1 = {"id": 20081, "name": "wheat flour","estimatedCostInCents": 142};
+// const ingredient2 = {"id": 18372,"name": "bicarbonate of soda", "estimatedCostInCents": 582};
+// const ingredient3 = {"id": 12345,"name": "fake crab", "estimatedCostInCents": 213};
+// const ingredient4 = {"id": 54321,"name": "ritz crackers", "estimatedCostInCents": 300};
 
-const ingredientData = [ingredient1, ingredient2, ingredient3, ingredient4]
+export const ingredientTestData = [
+  {"id": 20081, "name": "wheat flour","estimatedCostInCents": 142},
+  {"id": 18372,"name": "bicarbonate of soda", "estimatedCostInCents": 582},
+  {"id": 12345,"name": "fake crab", "estimatedCostInCents": 213},
+  {"id": 54321,"name": "ritz crackers", "estimatedCostInCents": 300}
+];
 
-export default ingredientData;
+// export default ingredientTestData;
