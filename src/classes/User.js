ab0982c6544ea57c8e8f9a6170eb18d892b301c0
Kat White <kat.white.a@gmail.com>
2021-05-21 12:20:31 -0600

    Add user-class test info

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,11 +1,21 @@
-// User class
-// Name
-// id
-// pantry - array (??? extension)
-// favoriteRecipes array
-// recipesToCook array 
-// It should have methods to:
-// Allow a user to favorite or unfavorite recipes (add to / remove from the user’s favoriteRecipes)
-// Filter my favoriteRecipes by one or more tags - RecipeRepository method?
-// Filter my favoriteRecipes by its name or ingredients - RecipeRepository method?
-// Decide to cook a recipe that week (add to recipesToCook)
+class User {
+  constructor(user) {
+    this.name = user.name
+    this.id = user.id
+    this.pantry = user.pantry
+    this.favoriteRecipes = []
+    this.recipesToCook = []
+  }
+
+  addFavoriteRecipe() {
+    
+  }
+
+  filterFavoriteRecipe() {
+    // by name, ingredient, tags
+  }
+
+  addToWeeklyMenu() {
+
+  }
+}
