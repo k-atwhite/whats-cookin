f10adfa608c5478fd6ddff30e033e8e2d22a3079
Lauren Kessell <lkessell1@gmail.com>
2021-05-22 16:24:09 -0400

    Refactor to compile ingredients in recipe repo

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,13 +1,16 @@
 import Recipe from './Recipe';
-
+import Ingredient from './Ingredient';
 
 class RecipeRepository {
   constructor(recipeData, ingredientsData) {
-    this.recipes = recipeData.map(recipe => new Recipe(recipe))
+    this.recipes = recipeData.map(recipe => new Recipe(recipe));
     this.ingredients = ingredientsData;
+
+    this.recipes.forEach(recipe => recipe.compileIngredients(ingredientsData));
   }
 
   filterTags(searchText) {
+    console.log(this.recipes[0].ingredients);
     return this.recipes.filter((recipe) => recipe.tags.includes(searchText))
   }
 
