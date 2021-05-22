a183c3940e496b3ebd1d3a1e177f53eb411079a2
Kat White <kat.white.a@gmail.com>
2021-05-22 15:57:57 -0600

    Pseudocode for searchbar

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,23 @@ console.log('Hello world');
 // Use the scripts.js file to add information to the DOM.
 // ITERATION 1
 // As a user, I should be able to view a list of all recipes.
-    // User clicks All Recipes Button (or dropdown) - 
+    //OPEN WINDOW - have all the necessary buttons. But also - just show all the recipes at once WITH the searchbar on top
+    // have a displayCharacters function - use map for each recipe object. And convert each object into innerHTML. 
+    // When user is typing into searchbox, get a reference from the searchbox - getElementById - to access the searchbox
+    
+    // call this const loadRecipes
+    // searchBar.addEventListener('keyup', () => {
+    // const searchText = e.target.value.toLowerCase();
+    //    console.log(e.target.value)
+    // const filteredRecipes = recipes.filter( (recipe) => {
+    //          call all three filtering methods(play with this)
+   //       })
+    // })
+
+    // event.target.value (that gets each key)
+    // create global variable for let recipes = []
+
+
     // click event listener runs event hander + hide/show function
     // HTML already has an "all recipe container' where all the recipes will appear
     // event handler - a render function, where ALL recipe data is displayed in the "all recipe container" via innerHTML
@@ -21,6 +37,10 @@ console.log('Hello world');
     // run getIngredientNames, getInstructions, getCost - Display!
 // As a user, I should be able to filter recipes by a single tags.
 // As a user, I should be able to search recipes by their name or ingredients.
+    // Create an unpopulated unordered list in HTML to dynamically fill with our search result recipes
+    // add littler search icon?
+    // jsFile is loaded
+    // load up all recipes (to where, recipe repo)
     // User types in something that perfectly matches our dataset
     // Clicks search
     // search event listner runs event handler that holds all three search functions (name, ingredients, tag)
